2d65ae96e12c3eac2443dd564502d3e5ac3ee3dd
illiao-wix <illiao@wix.com>
2020-03-24 13:05:16 +0200

    first commit

---
+++ src/pages/PAGE_A.c1dmp/pageElements.d.ts
@@ -0,0 +1,7 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#button1": $w.Button;
+  "#header1": $w.Header;
+  "#footer1": $w.Footer;
+  "#text1": $w.Text;
+}
---
+++ src/pages/site/pageElements.d.ts
@@ -0,0 +1,5 @@
+type PageElementsMap = {
+  "#header1": $w.Header;
+  "#footer1": $w.Footer;
+  "#text1": $w.Text;
+}
